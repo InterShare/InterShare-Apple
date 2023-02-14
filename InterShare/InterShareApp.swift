8733b7e4beb3e06535b987765833409889cae9e3
Julian Baumann <git@julba.de>
2023-02-14 12:07:40 +0100

    Initial Commit

---
+++ InterShare/InterShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterShareApp.swift
+//  InterShare
+//
+//  Created by Julian Baumann on 14.02.23.
+//
+
+import SwiftUI
+
+@main
+struct InterShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
